a52d8d27606eb7f59a76a27f688ba9ffa8fd6f8c
John Hamilton Willis Jr <JohnWillisJunior@gmail.com>
2018-12-28 19:46:37 -0600

    Removed extraneous comments

--- enable_sr.v
+++ enable_sr.v
@@ -1,24 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 11/01/2017 02:06:11 PM
-// Design Name: 
-// Module Name: enable_sr
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module enable_sr(
  input refreshClk, //input clock
