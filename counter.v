cce8178e941d0dc6ba0fb95fee573bef857d3b50
John Hamilton Willis Jr <JohnWillisJunior@gmail.com>
2018-12-28 19:47:05 -0600

    Removed extraneous comments

--- counter.v
+++ counter.v
@@ -1,24 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 11/01/2017 02:08:10 PM
-// Design Name: 
-// Module Name: counter
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module counter(
  input button_n, //start and stop button.
